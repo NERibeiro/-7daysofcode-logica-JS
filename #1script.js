a1f7c6a248bd8993caad3ad1102f5690dafb92c1
NERibeiro <renanpvh@hotmail.com>
2024-06-24 09:52:28 -0400

    att

--- #1script.js
+++ #1script.js
@@ -4,7 +4,7 @@ let numeroTrinta = 30
 let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
-/*
+
 if (numeroUm ==  stringUm) {
     console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
   } else {
@@ -15,7 +15,7 @@ if (numeroUm ==  stringUm) {
     console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
   } else {
     console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
-  }*/
+  }
   
   if (numeroDez == stringDez) {
     console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
